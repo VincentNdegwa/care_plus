a82328496f25dd3a8e8c6ce1a6bca2d2b85b030d
VincentNdegwa <ndegwavincent7@gmail.com>
2024-11-11 22:41:55 +0300

    "Updated CORS configuration to allow additional origin and added cache clearing steps to deployment script"

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -9,8 +9,21 @@ php artisan key:generate --show
 echo "Caching config..."
 php artisan config:cache
 
+echo "Clearing cache..."
+php artisan cache:clear
+
+echo "Clearing sessions..."
+php artisan session:clear
+
+echo "Clearing view cache..."
+php artisan view:clear
+
+echo "Clearing compiled views..."
+php artisan view:clear
+
 echo "Caching routes..."
 php artisan route:cache
+
 
 echo "Running migrations..."
 php artisan migrate --force
