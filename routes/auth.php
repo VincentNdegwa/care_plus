063f3e63bc1ec2834ec40ab134c7dd2c41eaa03d
VincentNdegwa <ndegwavincent7@gmail.com>
2024-11-12 15:49:25 +0300

    change the cors settings

--- routes/auth.php
+++ routes/auth.php
@@ -3,12 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\AuthenticateUserController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\RegisterUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 
@@ -18,13 +16,13 @@ Route::prefix("/v1")->group(function () {
 
     Route::post('/login', [AuthenticateUserController::class, 'create']);
 
-    // Route::post('/forgot-password', [PasswordResetLinkController::class, 'store']);
+    Route::post('/forgot-password', [PasswordResetLinkController::class, 'store']);
 
-    // Route::post('/reset-password', [NewPasswordController::class, 'store']);
+    Route::post('/reset-password', [NewPasswordController::class, 'store']);
 
-    // Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class);
+    Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class);
 
-    // Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store']);
+    Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store']);
 
     Route::middleware(['auth:sanctum'])->group(function () {
         Route::post('/logout', [AuthenticateUserController::class, 'destroy']);
