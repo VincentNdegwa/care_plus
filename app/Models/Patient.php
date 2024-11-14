00a98d9f9788ff5b24b0ed6e4a5900a199a8a6a4
VincentNdegwa <ndegwavincent7@gmail.com>
2024-11-15 00:34:07 +0300

    patient module

---
+++ app/Models/Patient.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Patient extends Model
+{
+    protected $table = "patients";
+    protected $fillable = [
+        "user_id",
+        "main_doctor_id",
+        "caregiver_id",
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, "user_id");
+    }
+
+    public function mainDoctor()
+    {
+        return $this->belongsTo(User::class, "main_doctor_id");
+    }
+
+    public function caregiver()
+    {
+        return $this->belongsTo(User::class, "caregiver_id");
+    }
+}
