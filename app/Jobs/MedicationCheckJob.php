f4f39fdf1c54a31abe3a93eb20549564b841c81d
VincentNdegwa <ndegwavincent7@gmail.com>
2025-01-17 16:43:07 +0300

    Enhance MedicationCheckJob to log processing details and handle exceptions during schedule processing

--- app/Jobs/MedicationCheckJob.php
+++ app/Jobs/MedicationCheckJob.php
@@ -25,16 +25,25 @@ class MedicationCheckJob implements ShouldQueue
      */
     public function handle(): void
     {
-        $nowTime = Carbon::now();
-        $ourLateTime = $nowTime->copy()->subHour();
+        try {
+            $nowTime = Carbon::now();
+            $ourLateTime = $nowTime->copy()->subHour();
 
-        $schedules = MedicationSchedule::whereBetween('dose_time', [$ourLateTime, $nowTime])
-            ->whereNull("processed_at")
-            ->get();
 
-        foreach ($schedules as $schedule) {
-            $schedule->update(["processed_at" => $nowTime]);
-            Log::info("Schedule " . $schedule);
+            $schedules = MedicationSchedule::where('dose_time', '>=', $ourLateTime)
+                ->where('dose_time', '<=', $nowTime)
+                ->whereNull("processed_at")
+                ->get();
+
+
+            foreach ($schedules as $schedule) {
+                Log::info("Processing schedule ID: " . $schedule->id);
+
+                $schedule->processed_at = $nowTime;
+                $schedule->save();
+            }
+        } catch (\Throwable $th) {
+            Log::error("Error: " . $th->getMessage());
         }
     }
 }
