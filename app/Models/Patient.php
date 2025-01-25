b94da4ff8299bab638bc1ec69309d4df1295a629
VincentNdegwa <ndegwavincent7@gmail.com>
2025-01-25 22:31:16 +0300

    feat: meds for today

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\Schedules\MedicationSchedule;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Patient extends Model
@@ -37,5 +39,24 @@ class Patient extends Model
     public function medications()
     {
         return $this->hasMany(Medication::class, 'patient_id');
+    }
+    public function todaySchedules()
+    {
+        $now = Carbon::now();
+        $start_day = $now->copy()->startOfDay();
+        $end_day = $now->copy()->endOfDay();
+
+        $medications = MedicationSchedule::whereBetween('dose_time', [$start_day, $end_day])
+            ->where("patient_id", $this->id)
+            ->with('medication')
+            ->get();
+
+        return [
+            "now" => $now,
+            "start_day" => $start_day,
+            "end_day" => $end_day,
+            "count" => $medications->count(),
+            "medications" => $medications,
+        ];
     }
 }
