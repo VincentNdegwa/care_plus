4ed9931b447421355ba75bdb9107d2fb3cd2c7d2
VincentNdegwa <ndegwavincent7@gmail.com>
2025-02-04 13:52:51 +0300

    fix: schedules setup

--- docker/start.sh
+++ docker/start.sh
@@ -3,11 +3,13 @@
 if [ "${QUEUE_WORKER}" = "true" ]; then
     echo "Starting Queue Worker..."
     exec php /var/www/html/artisan queue:work --tries=3 --timeout=90
+    exit 0
 fi
 
 if [ "${SCHEDULER}" = "true" ]; then
     echo "Starting Scheduler..."
     exec php /var/www/html/artisan schedule:work
+    exit 0
 fi
 
 # If no queue or scheduler, start PHP-FPM
