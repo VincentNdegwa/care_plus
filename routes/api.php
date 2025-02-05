e8304d041217e830c8855f9246536e969de9fe94
VincentNdegwa <ndegwavincent7@gmail.com>
2025-02-05 23:57:22 +0300

    fix: remove authentication middleware from broadcasting routes

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CareProvider\FetchCareProvidersController;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::routes(['middleware' => ['auth:sanctum']]);
+Broadcast::routes();
 Route::prefix('v1')->group(function () {
     Route::get('care-providers', [FetchCareProvidersController::class, 'fetchAll']);
     Route::get('patients/{patient_id}/doctors', [FetchCareProvidersController::class, 'fetchPatientDoctors']);
