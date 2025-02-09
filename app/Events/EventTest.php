fe137c5e3c44163bf7009989d9fabf5d35b6cf42
VincentNdegwa <ndegwavincent7@gmail.com>
2025-02-09 14:20:22 +0300

    refactor: update EventTest to use array for medication schedule and simplify broadcast data

--- app/Events/EventTest.php
+++ app/Events/EventTest.php
@@ -21,27 +21,45 @@ class EventTest implements ShouldBroadcast
      * Create a new event instance.
      */
     public $schedule;
-    public $medicationSchedule;
+
     public function __construct()
     {
-        $schedule = MedicationSchedule::create([
-            'medication_id' => 5,
-            'patient_id' => 2,
-            // 'medication_id'=> 1,
-            // 'patient_id'=> 1,
-            'dose_time' => '2025-02-13 05:24:00',
-            'status' => 'Pending'
-        ]);
+        $schedule = [
+            "id" => 26,
+            "medication_id" => 1,
+            "patient_id" => 1,
+            "dose_time" => "2025-02-08 15:11:21",
+            "processed_at" => "2025-02-08 15:12:04",
+            "status" => "Pending",
+            "taken_at" => null,
+            "second_notification_sent" => 1,
+            "created_at" => "2025-02-07T18:11:21.000000Z",
+            "updated_at" => "2025-02-08T17:12:06.000000Z",
+            "medication" => [
+                "id" => 1,
+                "patient_id" => 1,
+                "diagnosis_id" => null,
+                "medication_name" => "Panadols",
+                "dosage_quantity" => "2",
+                "dosage_strength" => "500mg",
+                "form_id" => 3,
+                "route_id" => 2,
+                "frequency" => "Three times a day",
+                "duration" => "3 days",
+                "prescribed_date" => "2025-02-07 17:33:25",
+                "doctor_id" => null,
+                "caregiver_id" => null,
+                "stock" => 20,
+                "active" => 1
+            ]
+        ];
 
         $this->schedule = $schedule;
-        $this->medicationSchedule = MedicationSchedule::with('medication')->find($schedule->id);
-
     }
 
     public function broadcastOn(): array
     {
-
-        $channel = 'medication.take.' . $this->schedule->patient_id;
+        $channel = 'medication.take.' . $this->schedule['patient_id'];
         Log::info("Broadcasting on channel", ['channel' => $channel]);
         return [
             new PrivateChannel($channel),
@@ -55,6 +73,6 @@ class EventTest implements ShouldBroadcast
 
     public function broadcastWith(): array
     {
-        return $this->medicationSchedule->toArray();
+        return $this->schedule;
     }
 }
