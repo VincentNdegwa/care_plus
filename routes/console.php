a168f24ab52f60c4fdb4936de49f7196340d56b6
VincentNdegwa <ndegwavincent7@gmail.com>
2025-02-16 13:23:35 +0300

    chore: remove unused Artisan commands and Telescope pruning from console routes

--- routes/console.php
+++ routes/console.php
@@ -3,17 +3,10 @@
 use App\Jobs\CheckSnoozeNotificationsJob;
 use App\Jobs\DeactivateMedication;
 use App\Jobs\MedicationCheckJob;
-use App\Jobs\TestJobNotification;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
 
 Schedule::job(new MedicationCheckJob)->everyMinute();
-Schedule::command('telescope:prune --hours=48')->daily();
 
 Schedule::job(new CheckSnoozeNotificationsJob)->everyMinute();
 Schedule::job(new DeactivateMedication)->daily();
