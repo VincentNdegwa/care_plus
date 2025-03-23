c2391db5ffbf3d06060e0a07b27dd1b249893a88
VincentNdegwa <ndegwavincent7@gmail.com>
2025-03-23 22:33:47 +0300

    feat: enhance Docker start script to optimize cache clearing and asset publishing

--- docker/start.sh
+++ docker/start.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
-# Clear cache and config before starting the workers
-php /var/www/html/artisan config:clear
-php /var/www/html/artisan cache:clear
+# Clear all caches first
+php /var/www/html/artisan optimize:clear
+
+rm -rf /var/www/html/storage/framework/views/*
+
+
+# Publish and build Filament assets
+php /var/www/html/artisan filament:assets
+php /var/www/html/artisan vendor:publish --tag=filament-config --force
+
+# Cache configurations
 php /var/www/html/artisan config:cache
-php /var/www/html/artisan route:clear
-php /var/www/html/artisan view:clear
+php /var/www/html/artisan route:cache
+php /var/www/html/artisan view:cache
+php /var/www/html/artisan storage:link
+
+# Set proper permissions
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache /var/www/html/public
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache /var/www/html/public
 
 if [ "${QUEUE_WORKER}" = "true" ]; then
     echo "Starting Queue Worker..."
