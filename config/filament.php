b9f126d040019112cdc7d4ad557579fae9a1bc69
VincentNdegwa <ndegwavincent7@gmail.com>
2025-03-23 18:18:53 +0300

    feat: add Filament configuration and update nginx settings for resource management

---
+++ config/filament.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'domain' => env('FILAMENT_DOMAIN', 'care.tech360.systems'),
+    'path' => env('FILAMENT_PATH', 'admin'),
+    'home_url' => env('FILAMENT_HOME_URL', '/admin'),
+    'auth' => [
+        'guard' => env('FILAMENT_AUTH_GUARD', 'web'),
+        'pages' => [
+            'login' => \Filament\Pages\Auth\Login::class,
+        ],
+    ],
+    'middleware' => [
+        'base' => [
+            'web',
+            \Illuminate\Cookie\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+        'auth' => [
+            \Filament\Http\Middleware\Authenticate::class,
+        ],
+    ],
+];
